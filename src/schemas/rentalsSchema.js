2a8e1b1633f736d4889080a0ad9a7ac1efdcd1ed
stonasss <luis.b.santos96@gmail.com>
2023-02-12 21:38:25 -0300

    ajuste ao schema rentals

--- src/schemas/rentalsSchema.js
+++ src/schemas/rentalsSchema.js
@@ -1,7 +1,7 @@
 import joi from 'joi'
 
 export const rentalsSchema = joi.object ({
-    customerId: joi.number().required(),
-    gameId: joi.number().required(),
+    customerId: joi.number().required().positive(),
+    gameId: joi.number().required().positive(),
     daysRented: joi.number().required().positive(),
 })
